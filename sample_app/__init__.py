dc701a5879bb447dc37feb4955b37ea136f50560
Cody <cody@quantifiedcode.com>
2016-08-14 04:49:24 +0000

    Migrated `%` string formating

--- sample_app/__init__.py
+++ sample_app/__init__.py
@@ -21,9 +21,9 @@ django-sample-app - A Django app with setup, unittests, docs and a demo site.
 VERSION = (0, 1) # following PEP 386
 
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
+    version = '{0!s}.{1!s}'.format(VERSION[0], VERSION[1])
     if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
+        version = '{0!s}.{1!s}'.format(version, VERSION[2])
     if VERSION[3] != 'f':
-        version = '%s%s%s' % (version, VERSION[3], VERSION[4])
+        version = '{0!s}{1!s}{2!s}'.format(version, VERSION[3], VERSION[4])
     return version
